c91c7793f7e8aeb944d7923cbb770fe769b8c184
James Towers <james@songdrop.com>
2016-06-29 09:47:06 +0100

    empty boilerplate

---
+++ includes/class-wp-post-projects-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       http://dropshop.io
+ * @since      1.0.0
+ *
+ * @package    Wp_Post_Projects
+ * @subpackage Wp_Post_Projects/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Wp_Post_Projects
+ * @subpackage Wp_Post_Projects/includes
+ * @author     James Towers <james@songdrop.com>
+ */
+class Wp_Post_Projects_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-wp-post-projects-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       http://dropshop.io
+ * @since      1.0.0
+ *
+ * @package    Wp_Post_Projects
+ * @subpackage Wp_Post_Projects/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Wp_Post_Projects
+ * @subpackage Wp_Post_Projects/includes
+ * @author     James Towers <james@songdrop.com>
+ */
+class Wp_Post_Projects_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
